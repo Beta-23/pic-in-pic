5c5732e37375456d9d2073d536cf94ed194cb559
Beta-23 <alfred.dominguez@gmail.com>
2020-09-09 15:37:06 -0700

    Test the selectMediaStream method

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Testing script!');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream () {
+	try {
+		testingError;
+	} catch (error) {
+		// Catch Error Here
+		console.log('error in the selectMediaStream method:', error);
+	}
+}
+
+// On Load
+selectMediaStream();
